601a0d281667d9d0bd235a28bea1c04976bf215b
MonikerSec <rymantheman@yahoo.com>
2022-10-24 03:11:49 -0800

    Added main.py and kv MuskTerm.kv

---
+++ main.py
@@ -0,0 +1,14 @@
+# Importing Kivy widgets and App class
+from kivy.uix.widget import Widget as Wg
+from kivy.app import App 
+
+# Defining the main widget
+class MainWidget(Wg):
+    pass
+
+# Defining the app class
+class MuskTermApp(App):
+    pass
+
+# Running the app instance
+MuskTermApp().run()
